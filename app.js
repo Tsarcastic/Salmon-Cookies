394e54f36138759d67ac3caeeac0f6141d6b583e
Brendan Davis <brendanmd@gmail.com>
2017-06-14 09:53:21 -0700

    Added employee calculations (without appending)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var hours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
-'16:00', '17:00', '18:00', '19:00', '20:00',];
+'16:00', '17:00', '18:00', '19:00'];
 var branches = [];
 
 var header = new function() {
@@ -16,7 +16,6 @@ var header = new function() {
   thEl.textContent = 'Today\'s Total';
   trEl.appendChild(thEl);
   cookies.appendChild(trEl)
-
 }
 
 function sellCookies() {
@@ -36,16 +35,18 @@ function Branch(name, min, max, avg) {
   this.avgph = avg;
   this.cookiesSoldEachHour = [];
   this.totalSold = 0;
+  this.empWork = 0
+  this.empNeed = [];
   branches.push(this);
 };
 
 Branch.prototype.sellCookies = function() {
   for (var i = 0; i < hours.length; i++) {
-      var bot = Math.ceil(this.minph);
-      var top = Math.floor(this.maxph);
-      var people = Math.floor(Math.random() * (top - bot + 1)) + bot;
-      this.totalSold = this.totalSold + Math.floor(this.avgph * people);
-      this.cookiesSoldEachHour.push(Math.floor(this.avgph * people));
+    var bot = Math.ceil(this.minph);
+    var top = Math.floor(this.maxph);
+    var people = Math.floor(Math.random() * (top - bot + 1)) + bot;
+    this.totalSold = this.totalSold + Math.floor(this.avgph * people);
+    this.cookiesSoldEachHour.push(Math.floor(this.avgph * people));
   }
 }
 
@@ -65,11 +66,11 @@ Branch.prototype.render = function() {
   cookies.appendChild(trEl)
 }
 
-  var pike = new Branch('1st and Pike', 23, 65, 6.3);
-  var seaTac = new Branch('Seatac Airport', 3, 24, 1.2);
-  var seaCen = new Branch('Seattle Center', 11, 38, 3.7);
-  var capHill = new Branch('Capitol Hill', 20, 38, 2.3);
-  var alki = new Branch('Alki', 2, 16, 4.6);
+var pike = new Branch('1st and Pike', 23, 65, 6.3);
+var seaTac = new Branch('Seatac Airport', 3, 24, 1.2);
+var seaCen = new Branch('Seattle Center', 11, 38, 3.7);
+var capHill = new Branch('Capitol Hill', 20, 38, 2.3);
+var alki = new Branch('Alki', 2, 16, 4.6);
 
 pike.sellCookies();
 pike.render();
@@ -82,49 +83,21 @@ capHill.render();
 alki.sellCookies();
 alki.render();
 
-//
-// var firPikUl = document.getElementById('firPik');
-//
-//
-//
-//   // var liEl = document.createElement('li');
-//   // liEl.textContent = hours[i] + ": " + soldCook + " cookies";
-//   // firPikUl.appendChild(liEl);
-//   // console.log(runningTotal)
-// }
-//
+var employees = new function() {
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
 
-//
-// var seaAirUl = document.getElementById('seaAir');
-//
-// for (var i = 0; i < hours.length; i++) { // For appending the TH elements
-//   var thEl = document.createElement('th');
-//   thEl.textContent = cookiesSold(this);
-//   seaAirUl.appendChild(liEl);
-// }
-//
+}
 
-//
-// var seaCenUl = document.getElementById('seaCen');
-//
-// for (var i = 0; i <hours.length; i++) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = hours[i] + ": " + cookiesSold(seaCen) + " cookies";
-//   seaCenUl.appendChild(liEl);
-// }
-//
-// var capHilUl = document.getElementById('capHil');
-//
-// for (var i = 0; i <hours.length; i++) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = hours[i] + ": " + cookiesSold(capHil) + " cookies";
-//   capHilUl.appendChild(liEl);
-// }
-//
-// var alkiUl = document.getElementById('alki');
-//
-// for (var i = 0; i <hours.length; i++) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = hours[i] + ": " + cookiesSold(alki) + " cookies";
-//   alkiUl.appendChild(liEl);
-// }
+Branch.prototype.empCalc = function() {
+for (var i = 0; i < this.cookiesSoldEachHour.length; i++) {
+  this.empWork = Math.floor(this.cookiesSoldEachHour[i] / 20)
+  if (this.empWork < 2) {
+    this.empWork = 2;
+  };
+  this.empNeed.push(this.empWork);
+  console.log(this.empWork);
+};
+}
