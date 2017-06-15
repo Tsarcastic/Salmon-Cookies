6c28b1fc2c96c75334955196f65dcacfaad63d59
Brendan Davis <brendanmd@gmail.com>
2017-06-15 12:21:18 -0700

    added footer functionality

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict'
+
 var hours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
 '16:00', '17:00', '18:00', '19:00'];
 var branches = [];
 
-var header = new function() { //Creates header
+function renderHeader() { //Creates header
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
   thEl.textContent = "Location";
@@ -49,7 +50,7 @@ Branch.prototype.sellCookies = function() {
     this.totalSold = this.totalSold + Math.floor(this.avgph * people);
     this.cookiesSoldEachHour.push(Math.floor(this.avgph * people));
   }
-}
+};
 
 Branch.prototype.render = function() {
   this.sellCookies();
@@ -66,45 +67,13 @@ Branch.prototype.render = function() {
   tdEl.textContent = this.totalSold;
   trEl.appendChild(tdEl);
   cookies.appendChild(trEl)
-}
+};
 
-function renderAll() {
+function renderBody() {
   for (var i = 0; i < branches.length; i++) {
     branches[i].render();
   };
-
-}
-
-// var employees = new function() { // Create the employee table
-//   var trEl = document.createElement('tr');
-//   var tdEl = document.createElement('td');
-//   tdEl.textContent = this.name;
-//   trEl.appendChild(tdEl);
-// }
-//
-// EmpCalc is solid
-// Branch.prototype.empCalc = function() { // Create the employee array
-//   for (var i = 0; i < this.cookiesSoldEachHour.length; i++) {
-//     this.empWork = Math.ceil(this.cookiesSoldEachHour[i] / 20);
-//     if (this.empWork < 2) {
-//       this.empWork = 2;
-//     };
-//     this.empNeed.push(this.empWork);
-//   };
-// }
-//
-// Branch.prototype.payroll = function() { //Appending the employee array
-//   var trEl = document.createElement('tr');
-//   var tdEl = document.createElement('td');
-//   tdEl.textContent = this.name;
-//   trEl.appendChild(tdEl);
-//   for (var i = 0; i < this.empNeed.length; i++) {
-//     tdEl = document.createElement('td');
-//     tdEl.textContent = this.empNeed[i];
-//     trEl.appendChild(tdEl);
-//   };
-//   cookies.appendChild(trEl)
-// }
+};
 
 var pike = new Branch('1st and Pike', 23, 65, 6.3);
 var seaTac = new Branch('Seatac Airport', 3, 24, 1.2);
@@ -112,25 +81,59 @@ var seaCen = new Branch('Seattle Center', 11, 38, 3.7);
 var capHill = new Branch('Capitol Hill', 20, 38, 2.3);
 var alki = new Branch('Alki', 2, 16, 4.6);
 
-renderAll();
+function renderFooter() {
+  var trEl = document.createElement('tr');
 
-// pike.empCalc()
-// pike.payroll()
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Hourly Totals';
+  trEl.appendChild(thEl);
+
+  var totalOfTotals = 0;
+  var hourlyTotal = 0;
+  for (var i = 0; i < hours.length; i++) {
+    console.log(i)
+    hourlyTotal = 0;
+    thEl = document.createElement('th');
+    for (var j = 0; j < branches.length; j++){
+      hourlyTotal += branches[j].cookiesSoldEachHour[i];
+      console.log("The hourly total is " + hourlyTotal + " on " + " iteration")
+      totalOfTotals += branches[j].cookiesSoldEachHour[i];
+    };
+    thEl.textContent = hourlyTotal;
+    trEl.appendChild(thEl);
+  };
+  thEl = document.createElement('thEl');
+  thEl.textContent = totalOfTotals;
+  trEl.appendChild(thEl);
+  cookies.appendChild(trEl);
+};
+
+function fullRender() {
+  renderHeader();
+  renderBody();
+  renderFooter();
+}
+
+fullRender();
+//End of footer
 
 var newLoc = document.getElementById('newBranch');
+var myTable = document.getElementById('cookies');
 
 function handleNewBranch(event) {
   event.preventDefault();
   var newName = event.target.name.value;
-  var newMin = event.target.min.value;
-  var newMax = event.target.max.value;
-  var newAvg = event.target.avg.value;
+  var newMin = parseInt(event.target.min.value);
+  var newMax = parseInt(event.target.max.value);
+  var newAvg = parseInt(event.target.avg.value);
   var newBranch = new Branch(newName, newMin, newMax, newAvg);
   newBranch.render();
   event.target.name.value = null;
   event.target.min.value = null;
   event.target.max.value = null;
   event.target.avg.value = null;
+  myTable.innerHTML = " ";
+  fullRender();
 };
 
 newLoc.addEventListener('submit', handleNewBranch);
