6e30d04f741bca5a7d4ec0408676f11be51859ab
Brendan Davis <brendanmd@gmail.com>
2017-06-15 18:21:26 -0700

    came from desktop
    changed some file names, added pictures, began earnest work on HTML & CSS

---
+++ Employees.js
@@ -0,0 +1,33 @@
+// var employees = new function() { // Create the employee table
+//   var trEl = document.createElement('tr');
+//   var tdEl = document.createElement('td');
+//   tdEl.textContent = this.name;
+//   trEl.appendChild(tdEl);
+// }
+//
+// EmpCalc is solid
+// Branch.prototype.empCalc = function() { // Create the employee array
+//   for (var i = 0; i < this.cookiesSoldEachHour.length; i++) {
+//     this.empWork = Math.ceil(this.cookiesSoldEachHour[i] / 20);
+//     if (this.empWork < 2) {
+//       this.empWork = 2;
+//     };
+//     this.empNeed.push(this.empWork);
+//   };
+// }
+//
+// Branch.prototype.payroll = function() { //Appending the employee array
+//   var trEl = document.createElement('tr');
+//   var tdEl = document.createElement('td');
+//   tdEl.textContent = this.name;
+//   trEl.appendChild(tdEl);
+//   for (var i = 0; i < this.empNeed.length; i++) {
+//     tdEl = document.createElement('td');
+//     tdEl.textContent = this.empNeed[i];
+//     trEl.appendChild(tdEl);
+//   };
+//   cookies.appendChild(trEl)
+// }
+
+// pike.empCalc()
+// pike.payroll()
