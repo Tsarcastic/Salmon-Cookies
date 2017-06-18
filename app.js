29684b7557d22e967c9ebd76345ac3b31183855c
Brendan Davis <BrendanMD@gmail.com>
2017-06-18 16:34:20 -0700

    crap

--- app.js
+++ app.js
@@ -4,11 +4,42 @@ var hours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:
 '16:00', '17:00', '18:00', '19:00'];
 var branches = [];
 
-function machenElement(type, content, parent) {//Still needs work
-  var newEl = document.createElement(type);
-  newEl.textContent = content;
-  parent.appendChild(type);
+// function machenElement(type, content, parent) {//Still needs work
+//   var newEl = document.createElement(type);
+//   newEl.textContent = content;
+//   parent.appendChild(type);
+//   }
+
+function Branch(name, min, max, avg) {
+  this.name = name;
+  this.minph = min;
+  this.maxph = max;
+  this.avgph = avg;
+  this.cookiesSoldEachHour = [];
+  this.totalSold = 0;
+  this.empWork = 0
+  this.empNeed = [];
+  branches.push(this);
+};
+
+var pike = new Branch('1st and Pike', 23, 65, 6.3);
+var seaTac = new Branch('Seatac Airport', 3, 24, 1.2);
+var seaCen = new Branch('Seattle Center', 11, 38, 3.7);
+var capHill = new Branch('Capitol Hill', 20, 38, 2.3);
+var alki = new Branch('Alki', 2, 16, 4.6);
+
+Branch.prototype.sellCookies = function() {
+  for (var i = 0; i < hours.length; i++) {
+    var bot = Math.ceil(this.minph);
+    var top = Math.floor(this.maxph);
+    var people = Math.floor(Math.random() * (top - bot + 1)) + bot;
+    this.totalSold = this.totalSold + Math.floor(this.avgph * people);
+    this.cookiesSoldEachHour.push(Math.floor(this.avgph * people));
   }
+};
+
+
+
 
 function renderHeader() { //Creates header
   var trEl = document.createElement('tr');
@@ -28,40 +59,8 @@ function renderHeader() { //Creates header
   cookies.appendChild(trEl)
 }
 
-function sellCookies() {
-  for (var i = 0; i < hours.length; i++) {
-    function cookiesSold(place) {
-      let bot = Math.ceil(this.minph);
-      let top = Math.floor(this.maxph);
-      let people = Math.floor(Math.random() * (top - bot + 1)) + bot;
-    };
-  };
-};
-
-function Branch(name, min, max, avg) {
-  this.name = name;
-  this.minph = min;
-  this.maxph = max;
-  this.avgph = avg;
-  this.cookiesSoldEachHour = [];
-  this.totalSold = 0;
-  this.empWork = 0
-  this.empNeed = [];
-  branches.push(this);
-};
-
-Branch.prototype.sellCookies = function() {
-  for (var i = 0; i < hours.length; i++) {
-    var bot = Math.ceil(this.minph);
-    var top = Math.floor(this.maxph);
-    var people = Math.floor(Math.random() * (top - bot + 1)) + bot;
-    this.totalSold = this.totalSold + Math.floor(this.avgph * people);
-    this.cookiesSoldEachHour.push(Math.floor(this.avgph * people));
-  }
-};
-
 Branch.prototype.render = function() {
-  this.sellCookies(); //Need to get rid of this - Recalculating totals every time it renders
+  // this.sellCookies(); //Need to get rid of this - Recalculating totals every time it renders
   var trEl = document.createElement('tr');
 
   var tdEl = document.createElement('td');
@@ -80,17 +79,33 @@ Branch.prototype.render = function() {
   cookies.appendChild(trEl)
 };
 
+
+
 function renderBody() {
   for (var i = 0; i < branches.length; i++) {
     branches[i].render();
   };
 };
 
-var pike = new Branch('1st and Pike', 23, 65, 6.3);
-var seaTac = new Branch('Seatac Airport', 3, 24, 1.2);
-var seaCen = new Branch('Seattle Center', 11, 38, 3.7);
-var capHill = new Branch('Capitol Hill', 20, 38, 2.3);
-var alki = new Branch('Alki', 2, 16, 4.6);
+// function sellCookies() {
+//   for (var i = 0; i < hours.length; i++) {
+//     function cookiesSold(place) {
+//       let bot = Math.ceil(this.minph);
+//       let top = Math.floor(this.maxph);
+//       let people = Math.floor(Math.random() * (top - bot + 1)) + bot;
+//     };
+//   };
+// };
+
+
+
+
+
+
+
+
+
+
 
 function renderFooter() {
   var trEl = document.createElement('tr');
@@ -139,6 +154,7 @@ function handleNewBranch(event) {//Creates new branch
     return alert('One or more of your fields is missing a value.')
   }
   else {
+  newBranch.sellCookies;
   newBranch.render();
   event.target.name.value = null;
   event.target.min.value = null;
@@ -151,64 +167,60 @@ function handleNewBranch(event) {//Creates new branch
 
 newLoc.addEventListener('submit', handleNewBranch);
 
-var employees = new function() { // Create the employee table
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  tdEl.textContent = this.name;
-  trEl.appendChild(tdEl);
-}
-
-
-Branch.prototype.empCalc = function() { // Create the employee array
-  for (var i = 0; i < this.cookiesSoldEachHour.length; i++) {
-    this.empWork = Math.ceil(this.cookiesSoldEachHour[i] / 20);
-    if (this.empWork < 2) {
-      this.empWork = 2;
-    };
-    this.empNeed.push(this.empWork);
-  };
-}
-
-Branch.prototype.payroll = function() { //Appending the employee array
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  tdEl.textContent = this.name;
-  trEl.appendChild(tdEl);
-  for (var i = 0; i < this.empNeed.length; i++) {
-    tdEl = document.createElement('td');
-    tdEl.textContent = this.empNeed[i];
-    trEl.appendChild(tdEl);
-  };
-  peoples.appendChild(trEl)
-}
-
-function prHeader() { //Creates employee header
-  var trEl = document.createElement('tr');
-  var thEl = document.createElement('th');
-  thEl.textContent = "Location";
-  trEl.append(thEl);
-  for (var i = 0; i < hours.length; i++) {
-    thEl = document.createElement('th');
-    thEl.textContent = hours[i];;
-    trEl.appendChild(thEl);
-  };
-  thEl = document.createElement('th');
-  thEl.textContent = 'Total Work Hours';
-  trEl.appendChild(thEl);
-  peoples.appendChild(trEl)
-}
-
-// pike.empCalc();
-// prHeader();
-// pike.payroll();
-
-for (var i = 0; i < branches.length; i++) {//Iterate through calculations
-  branches[i].empCalc();
-  console.log(branches[i] + 'needs' + 'employees')
-};
-
-prHeader();//Creates Header - Working great!
-
-for (var i = 0; i < branches.length; i++) { //Iterate through appending
-  branches[i].payroll();
-}
+// var employees = new function() { // Create the employee table
+//   var trEl = document.createElement('tr');
+//   var tdEl = document.createElement('td');
+//   tdEl.textContent = this.name;
+//   trEl.appendChild(tdEl);
+// }
+//
+//
+// Branch.prototype.empCalc = function() { // Create the employee array
+//   for (var i = 0; i < this.cookiesSoldEachHour.length; i++) {
+//     this.empWork = Math.ceil(this.cookiesSoldEachHour[i] / 20);
+//     if (this.empWork < 2) {
+//       this.empWork = 2;
+//     };
+//     this.empNeed.push(this.empWork);
+//   };
+// }
+//
+// Branch.prototype.payroll = function() { //Appending the employee array
+//   var trEl = document.createElement('tr');
+//   var tdEl = document.createElement('td');
+//   tdEl.textContent = this.name;
+//   trEl.appendChild(tdEl);
+//   for (var i = 0; i < this.empNeed.length; i++) {
+//     tdEl = document.createElement('td');
+//     tdEl.textContent = this.empNeed[i];
+//     trEl.appendChild(tdEl);
+//   };
+//   peoples.appendChild(trEl)
+// }
+//
+// function prHeader() { //Creates employee header
+//   var trEl = document.createElement('tr');
+//   var thEl = document.createElement('th');
+//   thEl.textContent = "Location";
+//   trEl.append(thEl);
+//   for (var i = 0; i < hours.length; i++) {
+//     thEl = document.createElement('th');
+//     thEl.textContent = hours[i];;
+//     trEl.appendChild(thEl);
+//   };
+//   thEl = document.createElement('th');
+//   thEl.textContent = 'Total Work Hours';
+//   trEl.appendChild(thEl);
+//   peoples.appendChild(trEl)
+// }
+//
+// for (var i = 0; i < branches.length; i++) {//Iterate through calculations
+//   branches[i].empCalc();
+//   console.log(branches[i] + 'needs' + 'employees')
+// };
+//
+// prHeader();//Creates Header - Working great!
+//
+// for (var i = 0; i < branches.length; i++) { //Iterate through appending
+//   branches[i].payroll();
+// }
